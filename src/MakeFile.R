faefad6c5dfb2656950ce2864b1c4c8a24d37858
Cory Nichols <patrick_nichols@apple.com>
2018-02-17 09:27:57 -0600

    makefiles

---
+++ src/MakeFile.R
@@ -0,0 +1,7 @@
+# makefile for Case Study 6
+
+
+setwd('~/DataScience/SMU_Data_Science/MSDS_QTW/MSDS_7333_CaseStudyUnit6/src/')
+
+# create cleaned training set
+source("cleanTrainingData.R")
---
+++ src/cleanTrainingData.R
@@ -0,0 +1,50 @@
+# this is the makefile for cleaning the training dataset used for analysis
+
+# CREATING A FUNCTION TO PREPARE THE DATA
+
+setwd('~/DataScience/SMU_Data_Science/MSDS_QTW/MSDS_7333_CaseStudyUnit6/Data/')
+
+readData = 
+  function(filename = 'offline.final.trace.txt', 
+           subMacs = c("00:0f:a3:39:e1:c0", "00:0f:a3:39:dd:cd", "00:14:bf:b1:97:8a",
+                       "00:14:bf:3b:c7:c6", "00:14:bf:b1:97:90", "00:14:bf:b1:97:8d",
+                       "00:14:bf:b1:97:81"))
+  {
+    txt = readLines(filename)
+    lines = txt[ substr(txt, 1, 1) != "#" ]
+    tmp = lapply(lines, processLine)
+    offline = as.data.frame(do.call("rbind", tmp), 
+                            stringsAsFactors= FALSE) 
+    
+    names(offline) = c("time", "scanMac", 
+                       "posX", "posY", "posZ", "orientation", 
+                       "mac", "signal", "channel", "type")
+    
+    # keep only signals from access points
+    offline = offline[ offline$type == "3", ]
+    
+    # drop scanMac, posZ, channel, and type - no info in them
+    dropVars = c("scanMac", "posZ", "channel", "type")
+    offline = offline[ , !( names(offline) %in% dropVars ) ]
+    
+    # drop more unwanted access points
+    offline = offline[ offline$mac %in% subMacs, ]
+    
+    # convert numeric values
+    numVars = c("time", "posX", "posY", "orientation", "signal")
+    offline[ numVars ] = lapply(offline[ numVars ], as.numeric)
+    
+    # convert time to POSIX
+    offline$rawTime = offline$time
+    offline$time = offline$time/1000
+    class(offline$time) = c("POSIXt", "POSIXct")
+    
+    # round orientations to nearest 45
+    offline$angle = roundOrientation(offline$orientation)
+    
+    return(offline)
+  }
+
+offlineTraining = readData()
+
+write.csv(offlineTraining, file="offlineTraining.csv")
